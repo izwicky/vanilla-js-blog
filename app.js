5ccd444b4a0ec0e7b0a1c757404cb54559cd26c4
Gabriel Rodriguez <gaby.e.rodriguez@gmail.com>
2022-05-26 16:44:05 -0500

    feat: add blog page

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 import { createNewsCard } from './js/news-list.js';
+import { blogRender } from './js/blog-page-render.js';
 
-createNewsCard();
+window.addEventListener('load', () => {
+   let app = document.querySelector('[data-app]');
+   let page = app.getAttribute('data-app');
+
+   if (page === 'index') {
+      createNewsCard();
+   }
+
+   if (page === 'blog') {
+      blogRender();
+   }
+});
